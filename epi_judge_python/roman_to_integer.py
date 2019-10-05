c92ab4106fd602fc1e27acc4baf6c283af13073e
Dilyan <rsmarinoff@gmail.com>
2019-10-05 13:28:58 +0300

    Added solution to 6.8

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -1,10 +1,21 @@
 from test_framework import generic_test
 
-
 def roman_to_integer(s):
-    # TODO - you fill in here.
-    return 0
-
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    special = {'V': ('I', 4), 'X': ('I', 9), 'L': ('X', 40),
+                'C': ('X', 90), 'D': ('C', 400), 'M': ('C', 900)}
+    result = 0
+    i = len(s) - 1
+    while i > 0:
+        if s[i] in special and special[s[i]][0] == s[i - 1]:
+            result += special[s[i]][1]
+            i -= 2
+        else:
+            result += values[s[i]]
+            i -= 1
+    if i == 0:
+        result += values[s[0]]
+    return result
 
 if __name__ == '__main__':
     exit(
