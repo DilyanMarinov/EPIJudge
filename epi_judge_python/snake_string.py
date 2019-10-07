fdfdc8f561355f27bf4c75e141b5fd03d36295bd
Dilyan P. Marinov <dilyan.marinov@MUSALA.COM>
2019-10-07 10:41:51 +0300

    Added solution to 6.10 snakestring

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -1,9 +1,10 @@
 from test_framework import generic_test
 
-
 def snake_string(s):
-    # TODO - you fill in here.
-    return ''
+    front = [s[i] for i in range(1, len(s), 4)]
+    mid = [s[i] for i in range(0, len(s), 2)]
+    back = [s[i] for i in range(3, len(s), 4)]
+    return ''.join(front) + ''.join(mid) + ''.join(back)
 
 
 if __name__ == '__main__':
