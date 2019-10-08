010d79c5ec543552de802b97e290b70a1ac8cffb
Dilyan P. Marinov <dilyan.marinov@MUSALA.COM>
2019-10-08 10:26:47 +0300

    Added solution to 7.11

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -1,8 +1,39 @@
 from test_framework import generic_test
 
+def find_middle(start):
+    end = start
+    middle = start
+    while end != None:
+        end = end.next
+        if end != None:
+            end = end.next
+        middle = middle.next
+    return middle
+
+
+def reverse_list(start):
+    nextNode = None
+    while start != None:
+        temp = start.next
+        start.next = nextNode
+        nextNode = start
+        start = temp
+    return nextNode
+
+
+def print_list(start):
+    while start != None:
+        print(start.data)
+        start = start.next
+
 
 def is_linked_list_a_palindrome(L):
-    # TODO - you fill in here.
+    mid_reversed = reverse_list(find_middle(L))
+    while mid_reversed != None:
+        if L.data != mid_reversed.data:
+            return False
+        mid_reversed = mid_reversed.next
+        L = L.next
     return True
 
 
