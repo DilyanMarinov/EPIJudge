997b928ec4bc29dd3af7f5a9a986a37099eed6e8
Dilyan Marinov <dilyan.marinov@musala.com>
2019-10-09 11:22:28 +0300

    Added solution to 8.4

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,10 +1,27 @@
 from test_framework import generic_test
-
+import re
 
 def shortest_equivalent_path(path):
-    # TODO - you fill in here.
-    return ''
-
+    stack = re.split("/+", path)
+    stack.reverse()
+    result = []
+    while len(stack) > 0:
+        if stack[-1] == '.' or stack[-1] == '':
+            stack.pop()
+        elif stack[-1] == '..':
+            token = stack.pop()
+            if len(result) == 0:
+                result.append(token)
+            elif result[-1] == '..':
+                result.append(token)
+            else:
+                result.pop()
+        else:
+            result.append(stack.pop())
+    output = '/'.join(result)
+    if path[0] == '/':
+        output = '/' + output
+    return output
 
 if __name__ == '__main__':
     exit(
